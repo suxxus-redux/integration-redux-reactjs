472199f07572a4caa99fbb700874e7309b1ac76e
suxxus <suxxus@outlook.com>
2016-06-07 07:15:00 +0200

    all

--- src/index.js
+++ src/index.js
@@ -1,54 +1,164 @@
 'use strict';
-var redux = require('redux');
-var createStore = redux.createStore;
-var bindActionCreators = redux.bindActionCreators;
-var initialState = { name: 'John' };
+(function(win) {
+    // var redux = require('redux'),
+    //     reactRedux = require('react-redux'),
+    //     react = require('react'),
+    //     reactDom = require('react-dom'),
+    //     //reactDOMServer = require('react-dom/server'),
+    //     Provider = reactRedux.Provider,
+    //     connect = reactRedux.connect,
+    //     createStore = redux.createStore,
+    //     bindActionCreators = redux.bindActionCreators;
 
-var SET_NAME = 'set.name';
+    var redux = win.Redux,
+        reactRedux = win.ReactRedux,
+        react = win.React,
+        reactDom = win.ReactDOM,
+        Provider = reactRedux.Provider,
+        createStore = redux.createStore,
+        connect = reactRedux.connect,
+        bindActionCreators = redux.bindActionCreators;
 
-var actionCreators = {
-    user: function(name) {
-        return {
-            type: SET_NAME,
-            name: name
+
+    var SET_NAME = 'set.name';
+    var initialState = { name: 'John' };
+
+    var actionCreators = {
+        user: function(name) {
+            return {
+                type: SET_NAME,
+                name: name
+            };
+        }
+    };
+
+    var reducer = function(state, action) {
+        var actions = {};
+        actions[SET_NAME] = function() {
+            return {
+                name: action.name
+            };
         };
-    }
-};
+        actions.default = function() {
+            return state;
+        };
 
-var reducer = function(state, action) {
-    var actions = {};
-    actions[SET_NAME] = function() {
+        var reduce = actions[action.type] || actions.default;
+        return reduce();
+    };
+
+    var store = createStore(reducer, initialState);
+
+    //----------------------------------------------------------------
+    var reactComponent = function(React) {
+        return function(value) {
+            return (React.createElement('p', {}, value.name));
+        }
+    };
+
+    var reactComponentContainer = function(
+        connect,
+        component,
+        mapStateToProps,
+        actionCreators,
+        mergeProps,
+        options) {
+        return connect(mapStateToProps, actionCreators, mergeProps, options)(component);
+    };
+
+    var app = function(React) {
+        return function(child) {
+            return function(store) {
+                return (React.createElement('div', { className: 'container' }, child(store)));
+            }
+        }
+    };
+
+    var mapStateToProps = function(state) {
+        console.log('state ', state);
+        return { name: state.name };
+    };
+
+    var mapDispatchToProps = function(dispatch) {
+        return { actions: bindActionCreators(actionCreators, dispatch) }
+    };
+
+    var especificActionCreator = function(dispatch) {
+        //return bindActionCreators(actionCreators.user, dispatch);
         return {
-            name: action.name
+            foo: function() {
+                console.log(121212121212);
+            }
         };
     };
-    actions.default = function() {
-        return state;
+
+    var createReactComponent = reactComponent(react);
+
+    var creacteReactComponentContainer = reactComponentContainer(
+        connect,
+        createReactComponent,
+        mapStateToProps,
+        actionCreators,
+        null, { pure: true });
+
+    var RootComponent = app(react)(creacteReactComponentContainer);
+    //console.log(new creacteReactComponentContainer({store:store}).WrappedComponent);
+
+    // CONNECT (options)
+    // console.log('=== Inject just dispatch and don`t listen to store ===');
+    // console.log(connect()(createReactComponent));
+    // console.log(' ---------------------------------------------------------------  ');
+    // console.log('=== Inject all action creators (user, ...) without subscribing to the store === ');
+    // console.log(connect(null, actionCreators)(createReactComponent));
+    // console.log(' ---------------------------------------------------------------  ');
+    // console.log('=== Inject dispatch and name === ');
+    // console.log(connect(mapStateToProps)(createReactComponent));
+    // console.log(' ---------------------------------------------------------------  ');
+    // console.log('=== Inject name and actionCreators === ');
+    // console.log(connect(mapStateToProps, actionCreators)(createReactComponent));
+    // console.log(' ---------------------------------------------------------------  ');
+    // console.log('=== Inject name and specificActionCreators === ');
+    // console.log(connect(mapStateToProps, especificActionCreator)(createReactComponent));
+
+    //console.log(RootComponent(store).props.children.props);
+
+    // var CommentBox = React.createClass({
+    //     displayName: 'CommentBox',
+    //     render: function(name) {
+    //         return (
+    //             React.createElement('div', { className: "commentBox" },
+    //                 "Hello, world! I am a CommentBox. " + name
+    //             )
+    //         );
+    //     }
+    // });
+
+
+    // PROVIDER
+    // store (Redux Store): The single Redux store in your application.
+    // children (ReactElement) The root of your component hierarchy.
+    var createProvider = function(React) {
+        return function(provider, store, children) {
+            return React.createElement(provider, { store: store },
+                React.createElement(children)
+            );
+        };
     };
 
-    var reduce = actions[action.type] || actions.default;
+    var myApp = createProvider(react)(Provider, store, CommentBox);
 
-    return reduce();
-};
+    console.log(' ---------------------------------------------------------------  ');
+    console.log(' === myApp === ');
+    reactDom.render(myApp, win.document.querySelector('#root'));
+    //reactDom.render(app(react)(createReactComponent)(initialState), win.document.querySelector('#root'));
+    //console.log(RootComponent);
+    console.log(' ---------------------------------------------------------------  ');
+    //    console.log(' --------------------- ' + createProvider(react)()  + ' -------------------------  ');
 
-var store = createStore(reducer, initialState);
+    console.log('App,  ', myApp);
+    // console.log(myApp.props.children.props.children);
+    // console.log(myApp);
+    // var tmp = reactDOMServer.renderToString(RootComponent(store));
+    // console.log(tmp);
 
-console.log('state.name should be John: ', store.getState().name === 'John');
-
-// dispatch an action
-store.dispatch(actionCreators.user('Marie Ann'));
-console.log('state should be Marie Ann: ', store.getState().name === 'Marie Ann');
-
-/*
-Turns an object whose values are action creators,
-into an object with the same keys,
-but with every action creator wrapped into
-a dispatch call so they may be invoked directly.
-*/
-var boundActionCreators = bindActionCreators(actionCreators, store.dispatch);
-var setUserNameToAlice = function(setUserName) {
-    // pure function
-    setUserName('Alice');
-};
-setUserNameToAlice(boundActionCreators.user);
-console.log('state should be Alice: ', store.getState().name === 'Alice');
+}(window));
